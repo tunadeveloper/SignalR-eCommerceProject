37e6205800ee9a85aa8c2a90e16cd2dbb3154946
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 10:18:14 +0300

    Updated

---
+++ eCommerce.UI/ViewComponents/Layout/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Layout
+{
+    public class _LayoutHeadComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
