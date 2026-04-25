ffd2eec7e8186497b6052ed8fd71eab398188d4d
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 12:32:16 +0300

    Updated

--- eCommerce.UI/ViewComponents/Home/_HomeFeatureComponentPartial.cs
+++ eCommerce.UI/ViewComponents/Home/_HomeFeatureComponentPartial.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.UI.DTOs.PromotionDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.UI.ViewComponents.Home
 {
-    public class _HomeFeatureComponentPartial:ViewComponent
+    public class _HomeFeatureComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomeFeatureComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetAsync("https://localhost:7224/api/Promotions");
+            if (result.IsSuccessStatusCode)
+            {
+                var jsonData = await result.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPromotionDTO>>(jsonData);
+                return View(values);
+            }
+            return View(null);
+        }
     }
 }
