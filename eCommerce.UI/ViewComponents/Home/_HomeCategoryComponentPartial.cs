3f9b8fc0f6faf47d5e7e7a45904fe2f29f561a35
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 12:04:34 +0300

    Updated

---
+++ eCommerce.UI/ViewComponents/Home/_HomeCategoryComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Home
+{
+    public class _HomeCategoryComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
---
+++ eCommerce.UI/ViewComponents/Home/_HomeFeatureComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Home
+{
+    public class _HomeFeatureComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
