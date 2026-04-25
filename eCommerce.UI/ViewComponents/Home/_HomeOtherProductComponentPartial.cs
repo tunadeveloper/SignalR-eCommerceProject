4064dd1cafd93c50795940f9a84e635f5beb479c
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 12:16:01 +0300

    Updated

---
+++ eCommerce.UI/ViewComponents/Home/_HomeOtherProductComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Home
+{
+    public class _HomeOtherProductComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
