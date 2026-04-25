ec83646327a0c86e7159d742fda2f3b6eb3f6d6e
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 14:06:39 +0300

    Updated

--- eCommerce.UI/ViewComponents/Home/_HomeOtherProductComponentPartial.cs
+++ eCommerce.UI/ViewComponents/Home/_HomeOtherProductComponentPartial.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.UI.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.UI.ViewComponents.Home
 {
     public class _HomeOtherProductComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomeOtherProductComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7224/api/Products");
+            var values = response.IsSuccessStatusCode ? JsonConvert.DeserializeObject<List<ResultProductDTO>>(await response.Content.ReadAsStringAsync())
+                : new List<ResultProductDTO>();
+
+            return View(values);
+        }
     }
 }
--- eCommerce.UI/ViewComponents/Home/_HomeServiceComponentPartial.cs
+++ eCommerce.UI/ViewComponents/Home/_HomeServiceComponentPartial.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.UI.DTOs.ServiceDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.UI.ViewComponents.Home
 {
     public class _HomeServiceComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _HomeServiceComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7224/api/Services");
+            var values = response.IsSuccessStatusCode ? JsonConvert.DeserializeObject<List<ResultServiceDTO>>(await response.Content.ReadAsStringAsync())
+                : new List<ResultServiceDTO>();
+
+            return View(values);
+        }
     }
 }
