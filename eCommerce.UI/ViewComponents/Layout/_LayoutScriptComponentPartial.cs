403e886809b1adeefc2e66e9ca5d5d8d2c5a8aad
tunadeveloper <tunabusiness25@gmail.com>
2026-04-25 10:43:03 +0300

    Updated

---
+++ eCommerce.UI/ViewComponents/Layout/_LayoutScriptComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Layout
+{
+    public class _LayoutScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
---
+++ eCommerce.UI/ViewComponents/Layout/_LayoutShopCartComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Layout
+{
+    public class _LayoutShopCartComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
