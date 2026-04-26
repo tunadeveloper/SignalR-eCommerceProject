ace84ff340440c7115669102b33227b9a093a292
tunadeveloper <tunabusiness25@gmail.com>
2026-04-26 13:25:25 +0300

    Updated

---
+++ eCommerce.UI/ViewComponents/Product/_CategoryListComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Product
+{
+    public class _CategoryListComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
---
+++ eCommerce.UI/ViewComponents/Product/_ProductListComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.UI.ViewComponents.Product
+{
+    public class _ProductListComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() => View();
+    }
+}
