309881ba41e9b9efeb6c47b7d9faebbff38ea6c8
tunadeveloper <tunabusiness25@gmail.com>
2026-04-26 13:32:21 +0300

    Updated

--- eCommerce.UI/ViewComponents/Product/_CategoryListComponentPartial.cs
+++ eCommerce.UI/ViewComponents/Product/_CategoryListComponentPartial.cs
@@ -1,9 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.UI.DTOs.CategoryDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.UI.ViewComponents.Product
 {
-    public class _CategoryListComponentPartial:ViewComponent
+    public class _CategoryListComponentPartial: ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CategoryListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7224/api/Categories");
+            var values = response.IsSuccessStatusCode ? JsonConvert.DeserializeObject<List<ResultCategoryDTO>>(await response.Content.ReadAsStringAsync())
+                : new List<ResultCategoryDTO>();
+
+            return View(values);
+        }
     }
 }
--- eCommerce.UI/ViewComponents/Product/_ProductListComponentPartial.cs
+++ eCommerce.UI/ViewComponents/Product/_ProductListComponentPartial.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerce.UI.DTOs.CategoryDTOs;
+using eCommerce.UI.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace eCommerce.UI.ViewComponents.Product
 {
     public class _ProductListComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ProductListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7224/api/Products");
+            var values = response.IsSuccessStatusCode ? JsonConvert.DeserializeObject<List<ResultProductDTO>>(await response.Content.ReadAsStringAsync())
+                : new List<ResultProductDTO>();
+
+            return View(values);
+        }
     }
 }
